c04bc3c85c38a442af0e29c2cb8d846ef755c3fc
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2026-05-07 12:26:31 +0600

    fix подключения version.php

--- install/index.php
+++ install/index.php
@@ -14,7 +14,7 @@ class beeralex_marking extends CModule
     public function __construct()
     {
         if (is_file(__DIR__ . '/version.php')) {
-            include_once(__DIR__ . '/version.php');
+            include (__DIR__ . '/version.php');
             $this->MODULE_ID           = 'beeralex.marking';
             $this->MODULE_VERSION      = $arModuleVersion['VERSION'];
             $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
